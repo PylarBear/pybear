e6dea657222aae130d9e6f5b90279ce0b7c0989e
Bill <WKSJR99@msn.com>
2024-12-15 13:53:52 -0500

    24_12_15_13_53_00 created base folder, put check_is_fitted & is_fitted in it

---
+++ src/pybear/base/__init__.py
@@ -0,0 +1,27 @@
+# Author:
+#         Bill Sousa
+#
+# License: BSD 3 clause
+#
+
+
+
+
+from ._check_is_fitted import check_is_fitted
+from ._is_fitted import is_fitted
+
+
+
+__all__ = [
+    'check_is_fitted',
+    'is_fitted'
+]
+
+
+
+
+
+
+
+
+
