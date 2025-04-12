1bd962243cda20b7e87db40808c8c178e05b5b93
BillSousa <WKSJR99@msn.com>
2025-04-12 15:55:52 -0400

    25_04_12_15_56_00 clean out old stuff in feature_extraction.text

--- src/pybear/feature_extraction/text/alphanumeric_str.py
+++ src/pybear/feature_extraction/text/alphanumeric_str.py
@@ -6,8 +6,6 @@
 
 
 
-
-
 def alphabet_str_lower():
     return ' abcdefghijklmnopqrstuvwxyz'
 
@@ -27,6 +25,24 @@ def number_str():
 def alphanumeric_str():
     return alphabet_str() + number_str()
 
+
+def states_incl_dc():
+    return [
+        'AK', 'AL', 'AR', 'AZ', 'CA', 'CO', 'CT', 'DC', 'DE', 'FL',
+        'GA', 'HI', 'IA', 'ID', 'IL', 'IN', 'KS', 'KY', 'LA', 'MA',
+        'MD', 'ME', 'MI', 'MN', 'MO', 'MS', 'MT', 'NC', 'ND', 'NE',
+        'NH', 'NJ', 'NM', 'NV', 'NY', 'OH', 'OK', 'OR', 'PA', 'RI',
+        'SC', 'SD', 'TN', 'TX', 'UT', 'VA', 'VT', 'WA', 'WI', 'WV',
+        'WY'
+    ]
+
+
+def states():
+    return [state for state in states_incl_dc() if state != 'DC']
+
+
+def lower_states():
+    return [state for state in states_incl_dc() if state not in ['DC','HI','AK']]
 
 
 
